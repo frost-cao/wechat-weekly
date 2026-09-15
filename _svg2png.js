a990cf549a0efee1be9ca5d04a082356f602eecc
frost-cao <18256302582@163.com>
2026-09-15 15:10:13 +0800

    chore: 清理误提交文件，新增 .gitignore 排除凭证与临时脚本

--- _svg2png.js
+++
@@ -1,9 +0,0 @@
-const sharp = require('sharp');
-const path = require('path');
-const dir = 'C:/Users/v_yiicao/WorkBuddy/20260413140616/wechat-weekly';
-sharp(path.join(dir, 'og-cover.svg'))
-  .resize(1200, 630)
-  .png({ compressionLevel: 9, palette: true })
-  .toFile(path.join(dir, 'og-cover.png'))
-  .then(info => console.log('PNG 生成成功:', info.format, info.width + 'x' + info.height, (info.size/1024).toFixed(1) + 'KB'))
-  .catch(err => { console.error('转换失败:', err.message); process.exit(1); });
